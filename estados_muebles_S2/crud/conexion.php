9cd12c5231654f19cafc60d1a76db87815c673d4
Mario <43281444+ManuCE@users.noreply.github.com>
2022-01-14 22:36:32 -0300

    Add files via upload

--- estados_muebles_S2/crud/conexion.php
+++ estados_muebles_S2/crud/conexion.php
@@ -1,7 +1,7 @@
 <?php 
 	class Conexion{
 		public function conectar(){
-			$conexion= sqlite_open("sqlite:estados_muebles_S2/estado_mueble.db");
+			$conexion= sqlite_open("sqlite:estados_muebles_S2/estados_muebles_S2/estado_mueble.db");
 			return $conexion;
 		}
 	}
